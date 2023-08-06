872e8f51244e58c8f9d08da7d4e12f0c1a92110e
Unduck <csc4592@gmail.com>
2023-08-06 21:33:04 +0900

    [ADD] add initial design system

---
+++ lib/utilities/color_style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorStyles {
+  static const Color darkGray = Color(0xff494949);
+  // ...
+}
---
+++ lib/utilities/text_style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static const hintTextStyle = TextStyle(fontStyle: FontStyle.italic);
+  static const noteTextStyle = TextStyle(fontSize: 12, color: Colors.black54);
+  static const memoTextStyle = TextStyle(
+      fontSize: 12, color: Colors.black45, fontStyle: FontStyle.italic);
+  // ...
+}
