33515fbcf17b8bda151915ab18b8a0f8b8fdfb9d
millejuice <potgusdavy@gmail.com>
2023-08-06 21:40:46 +0900

    [FIX] 스케쥴모델값 작성

---
+++ lib/model/schedule_model.dart
@@ -0,0 +1,18 @@
+class ScheduleModel{
+  Map<String,int> attend;
+  String title;
+  String description;
+  DateTime dueDate;
+  String place;
+  String part;
+  bool previous;
+  ScheduleModel(
+    this.attend,
+    this.title,
+    this.description,
+    this.dueDate,
+    this.place,
+    this.part,
+    this.previous
+  );
+}
