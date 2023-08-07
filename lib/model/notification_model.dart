d256a936cbbcd9bb69e9e968f2e4302fb3ed1a4a
millejuice <potgusdavy@gmail.com>
2023-08-07 14:13:20 +0900

    [ADD] notification_model.dart

---
+++ lib/model/notification_model.dart
@@ -0,0 +1,12 @@
+class NotificationModel{
+  bool onOff;
+  String title;
+  String content;
+  DateTime time;
+  NotificationModel(
+    this.onOff,
+    this.title,
+    this.content,
+    this.time
+  );
+}
