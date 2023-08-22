95711046c4c1afd254657ca24347783d0cfe77fc
Unduck <csc4592@gmail.com>
2023-08-23 05:12:19 +0900

    [ADD] 화면 사이즈 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pard_app/firebase_options.dart';
 import 'package:pard_app/my_app.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
