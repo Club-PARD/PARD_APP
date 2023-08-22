5e14c00d48ac1ff0eeb78580e3b891a872378ce7
Unduck <csc4592@gmail.com>
2023-08-23 01:29:35 +0900

    [UPDATE] init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pard_app/firebase_options.dart';
 import 'package:pard_app/my_app.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
