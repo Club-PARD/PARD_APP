3ec0210c3b656abdb8cac1617c0f9bef489b2b74
Unduck <csc4592@gmail.com>
2023-08-23 01:26:15 +0900

    [UPDATE] change to getx route

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:pard_app/main.dart';
+import 'package:get/get.dart';
+import 'package:pard_app/Views/my_point_view.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: () => MyPointScreen()),
+        // GetPage(name: '/first', page: () => FirstNamedPage()),
+        // GetPage(name: '/second', page: () => SecondNamedPage()),
+      ],
     );
   }
 }
