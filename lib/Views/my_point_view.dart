15f4b6337d722db50e37aa689e06a998c62e6758
Unduck <csc4592@gmail.com>
2023-08-24 15:59:47 +0900

    [UPDATE] my_point_view.dart

--- lib/Views/my_point_view.dart
+++ lib/Views/my_point_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:pard_app/component/pard_appbar.dart';
 import 'package:pard_app/controllers/point_controller.dart';
@@ -10,40 +11,27 @@ class MyPointScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
       appBar: PardAppBar(
         '내 점수',
       ),
       body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                pointController.getUserUid();
-              },
-              child: Text('uid 가져오기 버튼'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                pointController.getUserPoints();
-              },
-              child: Text('point 가져오기 버튼'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                pointController.getUserPoints();
-                pointController.getUserDataAndPoints();
-              },
-              child: Text('point model 버튼'),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('버튼'),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('버튼'),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24.0,
+            vertical: 32.0,
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                width: 229.w,
+                height: 36.h,
+                child: Image(
+                  image: AssetImage('assets/images/pardnershipTop3.png'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
