1f071a7b97c5dae9ac93f77998241f8bec0a17ce
Unduck <csc4592@gmail.com>
2023-08-30 19:01:03 +0900

    [UPDATE] initialize Local DateFormat

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:pard_app/controllers/bottombar_controller.dart';
 import 'package:pard_app/firebase_options.dart';
 import 'package:pard_app/my_app.dart';
@@ -9,10 +10,12 @@ import 'package:pard_app/my_app.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await initializeDateFormatting('ko_KR', null);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-    final BottomBarController bottomBarController = Get.put(BottomBarController());
+  final BottomBarController bottomBarController =
+      Get.put(BottomBarController());
 
   runApp(const MyApp());
 }
