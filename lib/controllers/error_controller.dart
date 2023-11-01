9c6b5f469fb5e1ae37ad3b7ce680dd541915aa55
Unduck <csc4592@gmail.com>
2023-11-01 23:12:48 +0900

    [ADD] 에러 컨트롤러 추가

--- lib/controllers/error_controller.dart
+++ lib/controllers/error_controller.dart
@@ -1,9 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 
+/// 에러 상태 관리 컨트롤러
 class ErrorController extends GetxController {
-  var _counter = 0.obs;
+  var deviceInfo = DeviceInfoPlugin();
+  var androidInfo;
+  var iosInfo;
 
-  int get counter => _counter.value;
+  /// 에러 로그 작성
+  Future<void> writeErrorLog(String log, String phoneNum, dynamic about,
+      {bool isWidgetLog = false}) async {
+    var errorLog;
+    if (GetPlatform.isAndroid) {
+      androidInfo = await deviceInfo.androidInfo;
+      if (kDebugMode) {
+        print('Running on ${androidInfo.model}');
+      }
+    } else {
+      iosInfo = await deviceInfo.iosInfo;
+      if (kDebugMode) {
+        print('Running on ${iosInfo.utsname.machine}');
+      }
+    }
 
-  void increment() => _counter.value++;
+    if (isWidgetLog) {
+      errorLog = [];
+      var parsedLogList = log.split(' ');
+      for (var i = 0; i < parsedLogList.length; i++) {
+        if (parsedLogList[i].startsWith('(package:pard_app/')) {
+          var temp = parsedLogList[i]
+              .toString()
+              .substring(1, parsedLogList[i].toString().length - 4);
+          errorLog.add(temp);
+        }
+      }
+    } else {
+      errorLog = log;
+    }
+
+    if ((isWidgetLog && errorLog.length > 1) || !isWidgetLog) {
+      await FirebaseFirestore.instance.collection('Errorlog').doc().set({
+        'phoneNum': phoneNum,
+        'about': about,
+        'log': errorLog,
+        'time': DateTime.now(),
+        'model':
+            GetPlatform.isAndroid ? androidInfo.model : iosInfo.utsname.machine
+      });
+    }
+  }
 }
