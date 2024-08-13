083e526470ca3c372d4f2663277d4b9243715d05
millejuice <potgusdavy@gmail.com>
2024-08-13 13:21:39 +0900

    REFACT : QR, 짜잘한 ui

--- lib/component/next_button.dart
+++ lib/component/next_button.dart
@@ -13,50 +13,55 @@ class NextButton extends StatelessWidget {
   const NextButton(
     this.title,
     this.route,
-    this.isNext, {super.key, 
+    this.isNext, {
+    super.key, 
     this.function,
   });
 
   @override
   Widget build(BuildContext context) {
-    Rx<FlutterSecureStorage> sStorage = const FlutterSecureStorage().obs;
+    // Initialize FlutterSecureStorage inside the build method
+    const FlutterSecureStorage sStorage = FlutterSecureStorage();
+
     return GestureDetector(
-        child: Container(
-          width: 327.w,
-          height: 56.h,
-          decoration: BoxDecoration(
-            gradient: isNext
-                ? LinearGradient(
-                    colors: [
-                      Theme.of(context).colorScheme.onSecondary,
-                      Theme.of(context).colorScheme.secondary,
-                    ],
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                  )
-                : null,
-            color: !isNext ? grayScale[30] : null,
-            borderRadius: BorderRadius.all(Radius.circular(8.w)),
-          ),
-          child: Center(
-            child: Text(
-              title,
-              style: TextStyle(
-                  fontSize: 18.sp,
-                  fontWeight: FontWeight.w600,
-                  height: 24.h / 18.h,
-                  color: whiteScale[100]),
+      onTap: () async {
+        if (isNext) {
+          if (function != null) {
+            function!();  // Call the passed function if not null
+          }
+          await sStorage.write(key: 'tos', value: 'agree'); // Save the agreement in secure storage
+          await Get.toNamed(route);  // Navigate to the specified route
+        }
+      },
+      child: Container(
+        width: 327.w,
+        height: 56.h,
+        decoration: BoxDecoration(
+          gradient: isNext
+              ? LinearGradient(
+                  colors: [
+                    Theme.of(context).colorScheme.onSecondary,
+                    Theme.of(context).colorScheme.secondary,
+                  ],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                )
+              : null,
+          color: !isNext ? grayScale[30] : null,
+          borderRadius: BorderRadius.all(Radius.circular(8.w)),
+        ),
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 18.sp,
+              fontWeight: FontWeight.w600,
+              height: 24.h / 18.h,
+              color: whiteScale[100],
             ),
           ),
         ),
-        onTap: () async {
-          if (isNext) {
-            if (function != null) {
-              function;
-            }
-            await sStorage.value.write(key : 'tos', value : 'agree'); // 이용약관 동의 저장
-            await Get.toNamed(route);
-          }
-        });
+      ),
+    );
   }
 }
